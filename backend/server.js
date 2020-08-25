3260545c75b1bf56bbdbc7c86ce9639f8e2d401d
debanshu <debanshu45@gmail.com>
2020-08-26 00:15:26 +0530

    db-connection

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+// Connect the database
+const database = process.env.ATLAS_URI;
+
+mongoose.connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(con => {
+    console.log('DB connection Successfully!');
+})
+.catch((err)=>{
+    console.warn(err)
+})
+
+
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
